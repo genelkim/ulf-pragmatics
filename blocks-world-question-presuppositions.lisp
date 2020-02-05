b6261c6fee3c6fd1b4795ee6533788f93f6635ae
bkane2 <Ben.kane@me.com>
2020-02-04 19:40:26 -0500

    Moved bw-specific normalization and negation functions out due to bug

--- blocks-world-question-presuppositions.lisp
+++ blocks-world-question-presuppositions.lisp
@@ -16,68 +16,82 @@
 ; by generating the relevant presuppositions, choosing the relevant one
 ; to reply to (currently just the first/only one in the list), and negating
 ; the ULF of the presupposition.
+; NOTE: Currently unused due to TTT/package bug
 ;
   (util:inout-intern (rawulf ulf :ulf-pragmatics :callpkg calling-package)
-    (negate-wh-question-presupposition (get-wh-question-presupposition ulf)))
+    (negate-wh-question-presupposition (normalize-wh-question-presupposition
+      (get-wh-question-presupposition ulf))))
 ) ; END get-response-to-bw-presupposition-failure
 
 
-(defun get-wh-question-presupposition (ulf)
-; ```````````````````````````````````````````
+(defun get-wh-question-presupposition (rawulf &key (calling-package nil))
+; `````````````````````````````````````````````````````````````````````````
 ; Gets relevant (task-specific) presupposition from a wh-question.
 ;
-  (cond
-    ((ttt:match-expr '(^* (at.p (what.d place.n))) ulf)
-      (ttt:apply-rule '(/ ((sub (at.p (what.d place.n)) _!) qmark?)
-                         (you.pro ((pres know.v) (ans-to (sub where.pq _!))))) ulf))
-    ((ttt:match-expr '(^* (of.p (what.d color.n))) ulf)
-      (if (ttt:match-expr '(^* most-n) ulf)
-        (car (mapcar #'normalize-wh-question-presupposition (infer-existence-from-bw-superlative ulf)))
-        (car (mapcar #'normalize-wh-question-presupposition (infer-existence-from-definite-n+preds ulf)))))
-    (t (car (mapcar #'normalize-wh-question-presupposition (infer-presuppositions-from-wh-q ulf)))))
+  (util:inout-intern (rawulf ulf :ulf-pragmatics :callpkg calling-package)
+    (cond
+      ((ttt:match-expr '(^* (at.p (what.d place.n))) ulf)
+        (ttt:apply-rule '(/ ((sub (at.p (what.d place.n)) _!) qmark?)
+                          (you.pro ((pres know.v) (ans-to (sub where.pq _!))))) ulf))
+      ((ttt:match-expr '(^* (of.p (what.d color.n))) ulf)
+        (if (ttt:match-expr '(^* most-n) ulf)
+          (get-bw-inferences-of-type ulf 'infer-existence-from-bw-superlative)
+          (get-bw-inferences-of-type ulf 'infer-existence-from-definite-n+preds)))
+      (t (get-bw-inferences-of-type ulf 'infer-presuppositions-from-wh-q))))
 ) ; END get-wh-question-presuppositions
 
 
-(defun normalize-wh-question-presupposition (ulf)
-; `````````````````````````````````````````````````````````````
-; Extracts result formula and applies task-specific normalization, including
-; (for now) fixing bugs, such as the presupposition having "something.d block.n"
-; instead of "some.d block.n".
+(defun get-bw-inferences-of-type (ulf type)
+; ```````````````````````````````````````````
+; Gets all inferences according to the function given in type.
 ;
-  (ttt:apply-rules
-    '((/ (something.d _!) (some.d _!))
-      (/ (some.d (ulf:adj? (! ulf:noun? (plur ulf:noun?)))) (some.d !))
-      (/ (nquan (somehow.mod-a many.a)) some.d)
-      (/ (I.pro ((past ulf:verb?) _! (adv-e (! (^* (some.d _!1))))))
-         (I.pro ((past ulf:verb?) _!)))
-      (/ ((a.d _!) ((tense? be.v) _*))
-         ((some.d _!) ((tense? be.v) _*))))
-    (output-ulf-normalization (result-formula ulf)))
-) ; END normalize-wh-question-presupposition
+  (car (mapcar (lambda (inf) (output-ulf-normalization (result-formula inf))) (funcall type ulf)))
+) ; END get-bw-inferences-of-type
 
 
-(defun negate-wh-question-presupposition (ulf)
-; `````````````````````````````````````````````
-; Negates a presupposition by changing "some"/"something" to "no"/"nothing" (in
-; the case of negations, we want to remove the double negative), or otherwise adding
-; a negation to the ULF.
-  (cond
-    ((ttt:match-expr '(^* (! some.d something.pro)) ulf)
-      (ttt:apply-rules
-        '((/ something.pro nothing.pro)
-          (/ (some.d _!) (no.d _!))
-          (/ (nothing.pro ((? (tense? do.aux-s)) not.adv-a (ulf:verb? _*))) (everything.pro ((tense? ulf:verb?) _*)))
-          (/ ((no.d _!) ((? (tense? do.aux-s)) not.adv-a (ulf:verb? _*))) ((every.d _!) ((tense? ulf:verb?) _*)))
-          (/ (nothing.pro ((tense? ulf:verb?) not.adv-a _*)) (everything.pro ((tense? ulf:verb?) _*)))
-          (/ ((no.d _!) ((tense? ulf:verb?) not.adv-a _*)) ((every.d _!) ((tense? ulf:verb?) _*)))
-          (/ (every.d (! (^* (plur ulf:noun?)))) (all.d !)))
-      ulf))
-    (t
-      (ttt:apply-rules
-        '((/ (_! ((tense? be.v) _*)) (_! ((tense? be.v) not.adv-a _*)))
-          (/ (_! ((tense? ulf:verb?) _*)) (_! ((tense? do.aux-s) not.adv-a (ulf:verb? _*)))))
-      ulf :max-n 1 :shallow t)))
-) ; END negate-wh-question-presupposition
+;; (defun normalize-wh-question-presupposition (ulf)
+;; ; `````````````````````````````````````````````````````````````
+;; ; Extracts result formula and applies task-specific normalization, including
+;; ; (for now) fixing bugs, such as the presupposition having "something.d block.n"
+;; ; instead of "some.d block.n".
+;; ;
+;;   (ttt:apply-rules
+;;     '((/ (something.d _!) (some.d _!))
+;;       (/ (some.d (ulf:adj? (! ulf:noun? (plur ulf:noun?)))) (some.d !))
+;;       (/ (nquan (somehow.mod-a many.a)) some.d)
+;;       (/ (I.pro ((past ulf:verb?) _! (adv-e (! (^* (some.d _!1))))))
+;;          (I.pro ((past ulf:verb?) _!)))
+;;       (/ ((a.d _!) ((tense? be.v) _*))
+;;          ((some.d _!) ((tense? be.v) _*))))
+;;     ulf)
+;; ) ; END normalize-wh-question-presupposition
+
+
+;; (defun negate-wh-question-presupposition (ulf)
+;; ; `````````````````````````````````````````````
+;; ; Negates a presupposition by changing "some"/"something" to "no"/"nothing" (in
+;; ; the case of negations, we want to remove the double negative), or otherwise adding
+;; ; a negation to the ULF.
+;;   (cond
+;;     ((ttt:match-expr '(^* (! some.d something.pro)) ulf)
+;;       (ttt:apply-rules
+;;         '((/ something.pro nothing.pro)
+;;           (/ (some.d _!) (no.d _!))
+;;           (/ (nothing.pro ((? (tense? do.aux-s)) not.adv-a (ulf:verb? _*))) (everything.pro ((tense? ulf:verb?) _*)))
+;;           (/ ((no.d _!) ((? (tense? do.aux-s)) not.adv-a (ulf:verb? _*))) ((every.d _!) ((tense? ulf:verb?) _*)))
+;;           (/ (nothing.pro ((tense? ulf:verb?) not.adv-a _*)) (everything.pro ((tense? ulf:verb?) _*)))
+;;           (/ ((no.d _!) ((tense? ulf:verb?) not.adv-a _*)) ((every.d _!) ((tense? ulf:verb?) _*)))
+;;           (/ (every.d (! (^* (plur ulf:noun?)))) (all.d !)))
+;;       ulf))
+;;     (t
+;;       (ttt:apply-rules
+;;         '((/ (_! ((tense? be.v) _*)) (_! ((tense? be.v) not.adv-a _*)))
+;;           (/ (_! ((tense? ulf:verb?) _*)) (_! ((tense? do.aux-s) not.adv-a (ulf:verb? _*)))))
+;;       ulf :max-n 1 :shallow t)))
+;; ) ; END negate-wh-question-presupposition
+
+
+
 
 
 ;; (mapcar (lambda (ulf)
